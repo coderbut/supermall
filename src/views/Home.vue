c9e1597524cae1db9962be450361869d3dd15e1f
coderbut <1186891236@qq.com>
2020-10-21 01:05:23 +0800

    '初始化项目'

---
+++ src/views/Home.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="home">
+    <van-search placeholder="商品搜索 共269万件好物" v-model="searchData" input-align="center"></van-search>
+    <van-swipe :autoplay="3000">
+      <van-swipe-item v-for="(image, index) in images" :key="index">
+        <img v-lazy="image"/>
+      </van-swipe-item>
+    </van-swipe>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Home',
+  data() {
+    return {
+      searchData: '',
+      images: [],
+      data: {}
+    }
+  },
+  computed:{
+
+  },
+  components: {},
+}
+</script>
